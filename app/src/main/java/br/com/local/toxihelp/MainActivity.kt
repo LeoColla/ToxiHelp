1786c70bd6862d1e424e8745887c85f3e0dcda0e
Leonardo Colla <leonardocolla123@gmail.com>
2025-12-09 09:56:21 -0300

    Na main ajustei as id dos botoes, na main kt, coloquei a operação de navegação para a tela seguinte criei a tela de introdução

--- app/src/main/java/br/com/local/toxihelp/MainActivity.kt
+++ app/src/main/java/br/com/local/toxihelp/MainActivity.kt
@@ -6,15 +6,35 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
+import android.widget.Button // Import necessário para referenciar o Button
+import android.content.Intent // Import necessário para usar Intent
+import br.com.local.toxihelp.Introducao // Import necessário para sua Activity de destino
+
+
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
+        val botaoIntroducao: Button = findViewById(R.id.b_introducao)
+        botaoIntroducao.setOnClickListener{
+            GoIntroducao()
+        }
+
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+    }
+
+    private fun GoIntroducao(){
+        // Variável de Intent por convenção com 'i' minúsculo
+        val intentIntroducao = Intent(this,Introducao::class.java)
+        startActivity(intentIntroducao)
+
     }
 }
