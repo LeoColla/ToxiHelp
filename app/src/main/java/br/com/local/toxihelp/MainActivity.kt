42bb1be814c1432fcff29b4a5f5c191307c552f5
Leonardo Colla <leonardocolla123@gmail.com>
2025-12-09 13:36:15 -0300

    introduzi a primeira tel ade categorias, e atela de creditos.

--- app/src/main/java/br/com/local/toxihelp/MainActivity.kt
+++ app/src/main/java/br/com/local/toxihelp/MainActivity.kt
@@ -5,10 +5,8 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-
-import android.widget.Button // Import necessário para referenciar o Button
-import android.content.Intent // Import necessário para usar Intent
-import br.com.local.toxihelp.Introducao // Import necessário para sua Activity de destino
+import android.widget.Button
+import android.content.Intent
 
 
 class MainActivity : AppCompatActivity() {
@@ -18,9 +16,13 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val botaoIntroducao: Button = findViewById(R.id.b_introducao)
-        botaoIntroducao.setOnClickListener{
-            GoIntroducao()
-        }
+        botaoIntroducao.setOnClickListener{ GoIntroducao() }
+
+        val botaoCredito: Button = findViewById(R.id.b_credito)
+        botaoCredito.setOnClickListener{ GoCreditos() }
+
+        val botaoCategorias: Button = findViewById(R.id.b_categoria)
+        botaoCategorias.setOnClickListener{ GoCategorias() }
 
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
@@ -32,9 +34,17 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun GoIntroducao(){
-        // Variável de Intent por convenção com 'i' minúsculo
         val intentIntroducao = Intent(this,Introducao::class.java)
         startActivity(intentIntroducao)
+    }
 
+    private fun GoCreditos(){
+        val intentCreditos = Intent(this,Creditos::class.java)
+        startActivity(intentCreditos)
+    }
+
+    private fun GoCategorias(){
+        val intentCategorias = Intent(this,Categorias::class.java)
+        startActivity(intentCategorias)
     }
 }
