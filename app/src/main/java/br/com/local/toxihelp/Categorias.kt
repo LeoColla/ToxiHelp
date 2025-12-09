16b640731f46844dd2d98853bf82a83b62b23dfd
Barbosa1342 <ga.briel.barbosa1342@gmail.com>
2025-12-09 15:28:59 -0300

    Recycle View funcionando

--- app/src/main/java/br/com/local/toxihelp/Categorias.kt
+++ app/src/main/java/br/com/local/toxihelp/Categorias.kt
@@ -23,7 +23,7 @@ class Categorias : AppCompatActivity() {
         enableEdgeToEdge()
         setContentView(R.layout.activity_categorias)
 
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main_categorias)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
